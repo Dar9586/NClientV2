224fb7d4017759e3bf169eff01b5dc57f0e2b14f
Dar9586 <atoppi2012@libero.it>
2018-09-30 20:20:09 +0200

    NClientV2 1.4.4 + Acra for debug

---
+++ app/src/main/java/com/dar/nclientv2/components/CrashApplication.java
@@ -0,0 +1,19 @@
+package com.dar.nclientv2.components;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.ACRA;
+import org.acra.BuildConfig;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraMailSender;
+
+@AcraCore(buildConfigClass = BuildConfig.class)
+@AcraMailSender(mailTo = "atoppi2013@gmail.com")
+public class CrashApplication extends Application{
+    @Override
+    protected void attachBaseContext(Context newBase){
+        super.attachBaseContext(newBase);
+        ACRA.init(this);
+    }
+}
