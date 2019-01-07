56eeca653c3bd86b0679ed6bc7220a743dcd8291
Dar9586 <atoppi2012@libero.it>
2019-01-07 14:53:38 +0100

    NClientV2 1.6.3 * Bug fixes

--- app/src/main/java/com/dar/nclientv2/components/CrashApplication.java
+++ app/src/main/java/com/dar/nclientv2/components/CrashApplication.java
@@ -9,10 +9,20 @@ import com.dar.nclientv2.async.database.DatabaseHelper;
 import com.dar.nclientv2.settings.Database;
 
 import org.acra.ACRA;
+import org.acra.ReportField;
 import org.acra.annotation.AcraCore;
 
 
-@AcraCore(buildConfigClass = BuildConfig.class,reportSenderFactoryClasses = MySenderFactory.class)
+@AcraCore(buildConfigClass = BuildConfig.class,reportSenderFactoryClasses = MySenderFactory.class,reportContent={
+        ReportField.PACKAGE_NAME,
+        ReportField.APP_VERSION_CODE,
+        ReportField.USER_CRASH_DATE,
+        ReportField.BUILD_CONFIG,
+        ReportField.APP_VERSION_NAME,
+        ReportField.STACK_TRACE,
+        ReportField.ANDROID_VERSION,
+        ReportField.LOGCAT
+})
 public class CrashApplication extends Application{
     @Override
     public void onCreate(){
