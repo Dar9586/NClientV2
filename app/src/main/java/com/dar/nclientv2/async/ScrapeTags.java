e75bdbdfea9fcd2c4d8697bc7561d9e767354887
Dar9586 <atoppi2012@libero.it>
2020-03-07 14:54:01 +0100

    NClientV2 2.1.1 * Refactoring * Bug fixes

--- app/src/main/java/com/dar/nclientv2/async/ScrapeTags.java
+++ app/src/main/java/com/dar/nclientv2/async/ScrapeTags.java
@@ -19,10 +19,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class ScrapeTags extends JobIntentService {
-    /*It is updated once a month*/
-    private static final String URL="https://violable-hats.000webhostapp.com/tags.json";
-    
-    private static final String ALTERNATIVE_URL="https://raw.githubusercontent.com/Dar9586/NClientV2/master/data/tags.json";
+
+    private static final String URL="https://raw.githubusercontent.com/Dar9586/NClientV2/master/data/tags.json";
     //check every 30day
     private static final long DIFFERENCE_TIME=30L*24*60*60;
     public ScrapeTags() {
@@ -34,7 +32,6 @@ public class ScrapeTags extends JobIntentService {
         try {
             if(new Date().getTime()-getApplicationContext().getSharedPreferences("Settings",0).getLong("lastSync",new Date().getTime())<DIFFERENCE_TIME)return;
             Response x=Global.client.newCall(new Request.Builder().url(URL).build()).execute();
-            if(x.code()!=200)x=Global.client.newCall(new Request.Builder().url(ALTERNATIVE_URL).build()).execute();
             if(x.code()!=200)return;
             JsonReader reader=new JsonReader(x.body().charStream());
             reader.beginArray();
